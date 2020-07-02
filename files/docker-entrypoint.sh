2dbb95144d9405b5bbccb28c2e63098c62fcbf87
vlombardino <67643138+vlombardino@users.noreply.github.com>
2020-07-02 16:36:47 -0700

    Add files via upload

---
+++ files/docker-entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#add user and password to tomcat-users.xml
+cp -f /etc/tomcat9/tomcat-users.xml.mod /etc/tomcat9/tomcat-users.xml
+sed -i "s|TOM_USER|$TOM_USER|g" /etc/tomcat9/tomcat-users.xml
+sed -i "s|TOM_PASS|$TOM_PASS|g" /etc/tomcat9/tomcat-users.xml
+
+#move and rename RemoteTM.war into folder webapps
+if [ -f "/tmp/RemoteTM.war" ]; then
+   mv /tmp/RemoteTM.war /var/lib/tomcat9/webapps/ROOT.war
+fi
+
+sed -i -e '9,11s/^/#/' -e '13s/^/#/' /usr/local/bin/docker-entrypoint.sh
+
+#start tomcat
+exec /usr/share/tomcat9/bin/catalina.sh run
